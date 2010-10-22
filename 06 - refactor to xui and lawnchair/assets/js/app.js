57f4c58ed012b9d179762022e3cae9b9841a1a65
Michael Brooks <michael@michaelbrooks.ca>
2010-10-21 20:58:26 -0700

    Update with BlackBerry 4.6 / 5.0 support.

--- 06 - refactor to xui and lawnchair/assets/js/app.js
+++ 06 - refactor to xui and lawnchair/assets/js/app.js
@@ -21,10 +21,21 @@ run(function () {
 		store.get('config', function(saved) {
 			if (saved) {
 				if (saved.map) {
-					x$('input[value=' + saved.map + ']').attr('checked',true);
+					// @blackberry
+					//
+					// While XUI's attribute selector works in general, it does 
+					// not work well with BlackBerry's form elements.
+					// More specifically, the 'checked' and 'selected' attributes
+					// that are common for radio, checkbox, and selection box elements.
+					//
+					// The alternative is to grab the raw element and explicitly set
+					// the attribute.
+					// 
+					x$('input[value=' + saved.map + ']')[0].checked = true;
 				}
 				if (saved.zoom) {
-					x$('input[name=zoom][value="' + saved.zoom + '"]').attr('checked',true);
+					// @blackberry
+					x$('input[name=zoom][value="' + saved.zoom + '"]')[0].checked = true;
 				}
 			}
 		});
@@ -35,7 +46,24 @@ run(function () {
             var map  = saved ? saved.map || ui('map') : ui('map')
             ,   zoom = saved ? saved.zoom || ui('zoom') : ui('zoom')
             ,   path = "http://maps.google.com/maps/api/staticmap?center=";
-			
+            
+            // @blackberry
+            //
+            // During the training session, the master branch of PhoneGap-BlackBerry
+            // was in the progress of updating geolocation to use the W3C geolocation spec.
+            // To properly demonstrate the geolocation API call of this demo, we stubbed
+            // out a lastPosition with a low timeout. The result is that the native
+            // geolocation call is bypassed and the lastPosition is immediatel returned.
+            //
+            navigator.geolocation.lastPosition = {
+                coords: {
+                    latitude:  '49.2485',
+                    longitude: '-123.1088'
+                },
+                timestamp: 1,
+                timeout:   0
+            };
+            
             navigator.geolocation.getCurrentPosition(function (position) {
                 var location = "" + position.coords.latitude + "," + position.coords.longitude;
                 path += location + "&zoom=" + zoom;
--- 06 - refactor to xui and lawnchair/assets/js/dsl.js
+++ 06 - refactor to xui and lawnchair/assets/js/dsl.js
@@ -1,22 +1,22 @@
 // the app method accepts a fn to invoke on init unobtrusively 
 var run = function(application) {
-    if (navigator.userAgent.indexOf('Browzr') > -1) {
-        // blackberry
-        setTimeout(application, 250)	
-    } else {
-        // attach to deviceready event, which is fired when phonegap is all good to go.
-        x$(document).on('deviceready', application, false);
-    }
+    // attach to deviceready event, which is fired when phonegap is all good to go.
+    //
+    // @blackberry now supports deviceready
+    //
+    x$(document).on('deviceready', application, false);
 }
 
 // throw our settings into a lawnchair
-, store = new Lawnchair({adaptor:'dom'})
+// @blackberry needs to use the BlackBerry Persistent Storage adaptor.
+, store = new Lawnchair({adaptor:'blackberry'})
 
 // shows id passed
 , display = function(id) {
     x$(["#welcome", "#map", "#settings"]).each(function(e, i) {
         var display = '#' + x$(e)[0].id === id ? 'block' : 'none';
-        x$(e).css({ 'display':display })
+        x$(e).css({ 'display':display });
+        window.scrollTo(0, 0);
     });
 }
 
@@ -33,9 +33,20 @@ var run = function(application) {
 
 // gets the value of the setting from the ui
 , ui = function(setting) {
-    var radio = x$('#settings_form')[0][setting];
+    // @blackberry
+    //
+    // BlackBerry struggles with forms and cannot treat the elements of a form
+    // as hash. Instead, we need to manually grab all of the form elements.
+    //
+    var radio = x$('#settings_form')[0].elements;
+    
     for (var i = 0, l = radio.length; i < l; i++) {
-        if (radio[i].checked)
+        // @blackberry
+        //
+        // Because we have grabbed all of the form elements, we need to 
+        // only consider the elements that have the 'setting' name.
+        //
+        if (radio[i].name === setting && radio[i].checked)
             return radio[i].value;
     }
 };
