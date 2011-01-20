3d12d68e9e9fb4eeabb694ba6291c86205cd90ad
Michael Brooks <michael@michaelbrooks.ca>
2011-01-20 11:07:05 -0800

    Add BlackBerry 6 support.
    Search for @blackberry for the
    required changes.

--- 06 - refactor to xui and lawnchair/assets/js/app.js
+++ 06 - refactor to xui and lawnchair/assets/js/app.js
@@ -35,7 +35,25 @@ run(function () {
             var map  = saved ? saved.map || ui('map') : ui('map')
             ,   zoom = saved ? saved.zoom || ui('zoom') : ui('zoom')
             ,   path = "http://maps.google.com/maps/api/staticmap?center=";
-			
+
+            // @blackberry
+            //
+            // The BlackBerry 6 simulator's GPS does not function properly,
+            // so `getCurrentPosition` is stubbed for this training demo.
+            //
+            // Please comment out if you deploy to a physical device.
+            //
+            navigator.geolocation.getCurrentPosition = function(callback) {
+                callback({
+                   coords: {
+                       latitude:  '49.2485',
+                       longitude: '-123.1088'
+                   },
+                   timestamp: 1,
+                   timeout:   0
+               });
+            };
+
             navigator.geolocation.getCurrentPosition(function (position) {
                 var location = "" + position.coords.latitude + "," + position.coords.longitude;
                 path += location + "&zoom=" + zoom;
